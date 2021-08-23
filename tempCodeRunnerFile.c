04eecaab5043477cff6bb7f1af8074b0abf2bbef
sanket <sanketbodke26@gmail.com>
2021-08-23 15:20:34 -0700

    Removed unwanted files

--- tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-// int main()
-// {
-//     int m1,m2,m3
-//     printf("Enter 3 subjects Marks")
-//     scanf("%d %d %d", &m1,&m2,&m3)
-//     printf("You entered %d %d %d", m1,m2,m3)
-// }
